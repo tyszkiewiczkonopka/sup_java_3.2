cb417ab206a23cf32b5dc7431d1213b06407b1ba
Magdalena <tyszkiewicz.konopqa@gmail.com>
2023-10-20 15:20:49 +0200

    First (working) version of streaming for properties with sensitive keys

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Map<String, String> properties = new HashMap();
+        properties.put("login", "Angelina");
+        properties.put("password", "angelinaInHollywood123");
+        properties.put("title", "Salt");
+        properties.put("url", "www.angelina.hollywood");
+        properties.put("errorMessage", "file has not been found");
+
+        String[] sensitiveKeys = {"login", "password", "url"};
+        Set<String> sensitiveKeysSet = new HashSet<>(List.of(sensitiveKeys));
+
+        properties.entrySet().stream()
+                .filter(entry -> sensitiveKeysSet.contains(entry.getKey()))
+                .forEach(entry -> entry.setValue("********"));
+
+        for (Map.Entry<String, String> entry : properties.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
